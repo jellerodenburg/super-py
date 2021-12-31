e11541c05d1589fb87fb9d1f38bace8ad1269afa
Jelle Rodenburg <jellerodenburg@gmail.com>
2021-12-31 15:56:51 +0100

    Add profit service

--- revenue_service.py
+++ revenue_service.py
@@ -5,6 +5,22 @@ from colorprint_service import prRed
 sales = get_sales_from_sold_csv()
 
 
+def get_revenue_in_euros(from_date, to_date):
+    total_sales_in_euros = 0.0
+    for sale in sales:
+        if sale.sell_date >= from_date and sale.sell_date < to_date:
+            total_sales_in_euros += float(sale.sell_price)
+    return total_sales_in_euros
+
+
+def get_number_of_sold_products(from_date, to_date):
+    number_of_products_sold = 0
+    for sale in sales:
+        if sale.sell_date >= from_date and sale.sell_date < to_date:
+            number_of_products_sold += 1
+    return number_of_products_sold
+
+
 def print_revenue_in_euros(from_date, to_date):
     all_args_ok = True
     if from_date == None:
@@ -15,12 +31,8 @@ def print_revenue_in_euros(from_date, to_date):
         all_args_ok = False
     if all_args_ok:
         if is_correct_date(from_date) and is_correct_date(to_date):
-            total_sales_in_euros = 0.0
-            number_of_products_sold = 0
-            for sale in sales:
-                if sale.sell_date >= from_date and sale.sell_date < to_date:
-                    total_sales_in_euros += float(sale.sell_price)
-                    number_of_products_sold += 1
+            total_sales_in_euros = get_revenue_in_euros(from_date, to_date)
+            number_of_products_sold = get_number_of_sold_products(from_date, to_date)
             revenue_rounded_two_decimals = round(total_sales_in_euros, 2)
             revenue_formatted_two_decimals = "{:.2f}".format(
                 revenue_rounded_two_decimals
